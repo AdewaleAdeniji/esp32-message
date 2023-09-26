fbefc1c4980ada1c82a3e8d086bba7c50e807a6b
Spiderman <40921989+AdewaleAdeniji@users.noreply.github.com>
2023-09-26 11:56:03 +0100

    Update index.ino

--- index.ino
+++ index.ino
@@ -3,8 +3,8 @@
 #include <LiquidCrystal_I2C.h>
 #include <WiFi.h>
 
-const char* ssid = "OLUWAFERANMI_5G";
-const char* password = "JESU1213";
+const char* ssid = "WIFI_NAME";
+const char* password = "WIFI_PASSWORD";
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Define the LCD's address, columns, and rows
 
